149647de7b3db9e5f31a8afd337a83a2f7c4dae4
L <ll6209@rit.edu>
2024-04-24 02:32:51 -0400

    override

--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
-import Image from "next/image";
-import Navbar from "./components/navbar";
-
 export default function Home() {
   return (
     <main>
-      home
-      {/* <Navbar></Navbar> */}
+
     </main>
   );
 }
