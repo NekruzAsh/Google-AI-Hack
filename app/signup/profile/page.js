1b8969515c97d5f35a8fb622703ba2f8f3abb307
L <ll6209@rit.edu>
2024-04-24 19:30:16 -0400

    push to update

---
+++ app/signup/profile/page.js
@@ -0,0 +1,61 @@
+import { Grid, Stack } from '@mui/material'
+import React from 'react'
+import HelpIcon from '@mui/icons-material/Help';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+
+const page = () => {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+        
+        <Stack spacing={3}>
+            <h1>Welcome! Let's create your profile</h1>
+            <p1>Let others get to know you better</p1>
+
+            <Grid container>
+                <Grid item >
+                    <h4>Add profile picture</h4>
+                </Grid>
+                <Grid item>
+                    <HelpIcon color='disabled'></HelpIcon>
+                </Grid>
+            </Grid>
+
+            <Grid spacing={1} container>
+                <Grid item>
+                    <button className='btn bg-white btn-circle border-dashed border-2 border-indigo-300 w-40 h-40'>
+                        <AddPhotoAlternateIcon></AddPhotoAlternateIcon>
+                    </button>
+                </Grid>
+
+                <Grid item>
+                    <button className='btn text-center'>Choose Image</button>
+                </Grid>
+            </Grid>
+
+
+            <Grid container>
+                <Grid item >
+                    <h4>Add your location</h4>
+                </Grid>
+                <Grid item>
+                    <HelpIcon color='disabled'></HelpIcon>
+                </Grid>
+            </Grid>
+
+            
+            <Grid container>
+                <Grid item>
+                    <button className='btn'>back</button>
+                </Grid>
+
+                <Grid item>
+
+                </Grid>
+            </Grid>
+        </Stack>
+ 
+    </div>
+  )
+}
+
+export default page
