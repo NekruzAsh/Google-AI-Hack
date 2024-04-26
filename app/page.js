88d45df67ca92be7f4fd2d2bb5e2138474878da8
L <ll6209@rit.edu>
2024-04-26 16:57:28 -0400

    update and push

--- app/page.js
+++ app/page.js
@@ -1,41 +1,51 @@
-import React from 'react'
-import Navbar from './Components/navbar'
-import { Grid, Stack } from '@mui/material'
+'use client'
+import React from 'react';
+import Navbar from './Components/navbar';
+import { Grid, Stack } from '@mui/material';
+import { useRouter } from 'next/navigation';
 
-const page = () => {
-  return (
-    <div className='h-screen w-screen'>
-        <Navbar/>
-        <Grid spacing={10} container className='h-full w-full flex justify-center items-center'>
-            <Grid item xs={4} >
-                <Stack spacing={3} textAlign="center" className='flex justify-center items-center'>
-                    <h2>Welcome to "App Name"!</h2>
-                    <button className='btn btn-neutral w-10/12'>Sign up with Google</button>
-                    <Grid container className='flex justify-center items-center'>
-                        <Grid item xs={4.5}>
-                            <div className='w-full h-0.5 bg-black'></div>
+
+const Page = () => {
+    const router = useRouter();
+
+    const handleRedirectEmail = () =>
+    {
+        router.push('/signup');
+    }
+
+    return (
+        <div className='h-screen w-screen'>
+            <Navbar/>
+            <Grid spacing={10} container className='h-full w-full flex justify-center items-center'>
+                <Grid item xs={4} >
+                    <Stack spacing={3} textAlign="center" className='flex justify-center items-center'>
+                        <h2>Welcome to "App Name"!</h2>
+                        <button className='btn btn-neutral w-10/12'>Sign up with Google</button>
+                        <Grid container className='flex justify-center items-center'>
+                            <Grid item xs={4.5}>
+                                <div className='w-full h-0.5 bg-black'></div>
+                            </Grid>
+                            <Grid item xs={1} >
+                                <p>or</p>
+                            </Grid>
+                            <Grid item xs={4.5}>
+                                <div className='w-full h-0.5 bg-black'></div>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={1} >
-                            <p1>or</p1>
-                        </Grid>
-                        <Grid item xs={4.5}>
-                            <div className='w-full h-0.5 bg-black'></div>
-                        </Grid>
-                    </Grid>
-                    <button className='btn w-10/12'>Continue with email</button>
-                
-                    <p>By creating an account you agree with our Term of Service, Privacy, Policy, and our default notification</p>
- 
-                    <p>Already have an account? Sign In</p>
-                </Stack>
+                        <button onClick={handleRedirectEmail} className='btn w-10/12'>Continue with email</button>
+                    
+                        <p>By creating an account you agree with our Term of Service, Privacy, Policy, and our default notification</p>
+    
+                        <p>Already have an account? Sign In</p>
+                    </Stack>
+                </Grid>
+    
+                <Grid item xs={6}>
+                    <img src="https://placehold.co/1000x800.svg" className='w-full h-full' alt=""></img>
+                </Grid>
             </Grid>
+        </div>
+    );
+};
 
-            <Grid item xs={6}>
-                <img src="https://placehold.co/1000x800.svg" className='w-full h-full' alt=""></img>
-            </Grid>
-        </Grid>
-    </div>
-  )
-}
-
-export default page
+export default Page;
