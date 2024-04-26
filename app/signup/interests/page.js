f7efcbeced5be8ae5fd6b0a38da441b6a51c7a9f
L <ll6209@rit.edu>
2024-04-25 21:24:42 -0400

    able to make the items centered

--- app/signup/interests/page.js
+++ app/signup/interests/page.js
@@ -1,6 +1,7 @@
 import { Grid, Stack } from '@mui/material'
 import React from 'react'
 import HelpIcon from '@mui/icons-material/Help';
+import { Textarea } from '@mui/joy';
 
 const page = () => {
 
@@ -11,51 +12,89 @@ const page = () => {
     
     {/* havent tested this feature, dont know if it works */}
   const interestsButtonCreate = () =>
-  {
-    return interestsList.map(e => {
-        <button className='btn btn-xs'>{e}</button>
-    });
-  }
+        interestsList.map(e => (
+            <button key={e} className='btn btn-xs '>{e}</button>
+        )
+    )
 
   return (
-    <div>
-        <Stack >
-            <h3>Almost done</h3>
-            <p>Final steps to complete your account</p>
+    <div className='h-screen w-screen flex justify-items-center justify-center items-center'>
+        <Stack spacing={5}>
+            <div>
+                <h2>Almost done</h2>
+            </div>
+            <div>
+                <p>Final steps to complete your account</p>
+            </div>
+
             <Grid container item id="second-row">   
-                <Grid item>                        
-                    <h4>3. About Me</h4>
-                </Grid>
-                <Grid item>
-                    <HelpIcon color='disabled'></HelpIcon>
+                <Grid container item xs={6}>
+                    <Grid item>                        
+                        <h4>3. About Me</h4>
+                    </Grid>
+                    <Grid item>
+                        <HelpIcon color='disabled'></HelpIcon>
+                    </Grid>
                 </Grid>
 
+                <Grid container item xs={6}  className='w-6/12 justify-center content-center'>
+                    <Grid item>                       
+                        <h4>4. Select 3 to 6 hobbies and interests</h4>
+                    </Grid>
+                    <Grid item>
+                        <HelpIcon color='disabled'></HelpIcon>
+                    </Grid>
+                </Grid>
 
-                <Grid item>                       
-                    <h4>4. Select 3 to 6 hobbies and interests</h4>
-                </Grid>
-                <Grid item>
-                    <HelpIcon color='disabled'></HelpIcon>
-                </Grid>
             </Grid>                
 
-            <Grid container item id="third-row">
+            <Grid container item id="third-row" >
                 <Grid item xs={6}>
                     <Textarea
+        
                         color="neutral"
-                        disabled={false}
-                        minRows={2}
+                        minRows={4}
                         placeholder="Talk a little about yourself. What do you want others to know about yourself?"
-                        size="lg"
+                        size="sm"
                         variant="outlined"
+                        style= {{width : "70%"}}
                         />
                 </Grid> 
 
-                <Grid item container id="interest-container">
-                    <Grid item xs={6}>
+                <Grid container item xs={6} id="interest-box" className='w-6/12 justify-center content-center'>
+                    <Grid item xs={8}>
                         {interestsButtonCreate()}
-                        {/* it's a nested container, where this one will be containing interests buttons*/}
                     </Grid>
+                </Grid>
+            </Grid>
+
+            <h4>5. Upload your photos</h4>
+
+            <Grid container >
+                <Grid item xs={6}> 
+                    <button className='btn'>Gallery</button>
+                    <button className='btn'>Gallery</button>
+                    <button className='btn'>Gallery</button>
+                    <button className='btn'>Gallery</button>
+                    <button className='btn'>Gallery</button>
+                </Grid>
+
+                <Grid container xs={3.65} item id="button-box" className='w-6/12 justify-items-center justify-center items-center'>
+                    <button className='btn ' >Choose Image</button>
+                </Grid>
+            </Grid>
+
+            <Grid container className=''>
+                <Grid item xs={7}>
+                    <p>3/4 steps</p>
+                </Grid>
+
+                <Grid item xs={1}>
+                    <button className='btn'>Back</button>
+                </Grid>
+
+                <Grid item xs ={1}>
+                    <button className='btn btn-neutral'>Continue</button>
                 </Grid>
             </Grid>
         </Stack>
