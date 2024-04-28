4066a0967352023c01e6be0e7b4ce477a9ecd71e
NekruzAsh <nekruzashrapov16@gmail.com>
2024-04-28 19:50:10 -0400

    updated landing page.

--- app/page.js
+++ app/page.js
@@ -8,7 +8,6 @@ import supabase from "./api/api";
 const Page = () => {
   const router = useRouter();
 
-
   const handleRedirectEmail = () => {
     router.push("/signup");
   };
@@ -49,6 +48,14 @@ const Page = () => {
                   Friendly
                 </span>
               </h2>
+              <p>
+                Set up an account as a volunteer or an elderly, list your
+                hobbies and interests and start matching with other people{" "}
+                <br />
+                Help out an elderly with loneliness and call them to
+                make them happy
+              </p>
+
               <button
                 onClick={checkCurrentUser}
                 className="bg-red-500 text-white btn btn-neutral w-10/12"
@@ -61,16 +68,21 @@ const Page = () => {
                 </Grid>
                 <Grid item xs={1}>
                   <p>or</p>
-
                 </Grid>
                 <Grid item xs={4.5}>
                   <div className="w-full h-0.5 bg-black"></div>
                 </Grid>
               </Grid>
-              <button onClick={handleRedirectEmail} className="btn bg-indigo-800 text-white w-10/12">
+              <button
+                onClick={handleRedirectEmail}
+                className="btn bg-indigo-800 text-white w-10/12"
+              >
                 Continue with email
               </button>
-              <button onClick={manuallyLogin} className="bg-gray-600 text-white btn w-10/12">
+              <button
+                onClick={manuallyLogin}
+                className="bg-gray-600 text-white btn w-10/12"
+              >
                 Manually login
               </button>
 
@@ -84,11 +96,7 @@ const Page = () => {
           </Grid>
 
           <Grid item xs={6}>
-            <img
-              src="/image.png"
-              className="w-full h-full"
-              alt=""
-            ></img>
+            <img src="/image.png" className="w-full h-full" alt=""></img>
           </Grid>
         </Grid>
       </div>
