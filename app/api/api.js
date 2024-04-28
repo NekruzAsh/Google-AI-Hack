bd006333baf58be5ce5dca643d252afc058d3ece
NekruzAsh <nekruzashrapov16@gmail.com>
2024-04-28 00:13:09 -0400

    updated key

--- app/api/api.js
+++ app/api/api.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = "https://rghvkjwhqpuihntomfui.supabase.co/";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnaHZrandocXB1aWhudG9tZnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxMjg3MTIsImV4cCI6MjAyOTcwNDcxMn0.QFMYuYta4uaFUHJb7u_xc4c76hezON_Dx_hDxIkhnNw";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
